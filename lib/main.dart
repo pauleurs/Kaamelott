1d28844e344da41c7276a2f920123db725db02d7
paul <paul-emile.leurs@epitech.eu>
2021-09-07 19:11:47 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Kaamelott",
-      initialRoute: '/second',
+      initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
         '/second': (context) => TeammatesModel(),
